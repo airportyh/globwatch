01f1fa07cb6b03d4ec4f89bdfb6233a70e5a88bf
airportyh <airportyh@gmail.com>
2012-10-27 05:59:15 -0400

    Refactoring.

--- globwatch.js
+++ globwatch.js
@@ -15,72 +15,76 @@ function clone(obj){
     return retval
 }
 
+var GW = clone(EventEmitter)
+
+GW.initialize = function(){
+    this.watchedFiles = {}
+    this.pendingTasks = 0
+}
+
+GW.add = function(thing){
+    if (thing.indexOf('*') !== -1){
+        this.addGlob(thing)
+    }else{
+        this.addFile(thing)
+    }
+}
+
+GW.prefix = function(pattern){
+    var idx = pattern.indexOf('/')
+    return idx === -1 ? '.' : pattern.substring(0, idx)
+}
+
+GW.addGlob = function(pattern){
+    var self = this
+    this.pushTask()
+    var prefixDir = this.prefix(pattern)
+    fs.readdir(prefixDir, function(err, files){
+        files.forEach(function(filename){
+            filename = path.join(prefixDir, filename)
+            if (minimatch(filename, pattern)){
+                self.add(filename)
+            }
+        })
+        self.popTask()
+    })
+    this.addFile(prefixDir)
+}
+
+GW.addFile = function(filename){
+    var self = this
+    if (this.watchedFiles[filename]) return
+    this.pushTask()
+    this.watchedFiles[filename] = fs.watch(filename, function(){
+        self.onFileAccessed(filename)
+    })
+    this.popTask()
+}
+
+GW.pushTask = function(){
+    this.pendingTasks++
+}
+
+GW.popTask = function(){
+    this.pendingTasks--
+    this.checkTasks()
+}
+
+GW.checkTasks = function(){
+    var self = this
+    if (this.pendingTasks === 0){
+        process.nextTick(function(){
+            self.emit('ready')
+        })
+    }
+}
+
+GW.onFileAccessed = function(filename){
+    this.emit('change', filename)
+}
+
 module.exports = function globwatch(){
-
-    var gw = clone(EventEmitter)
-
-    gw.watchedFiles = {}
-
-    gw.pendingTasks = 0
-
-    gw.add = function(thing){
-        if (thing.indexOf('*') !== -1){
-            gw.addGlob(thing)
-        }else{
-            gw.addFile(thing)
-        }
-    }
-
-    gw.prefix = function(pattern){
-        var idx = pattern.indexOf('/')
-        return idx === -1 ? '.' : pattern.substring(0, idx)
-    }
-
-    gw.addGlob = function(pattern){
-        gw.pushTask()
-        var prefixDir = gw.prefix(pattern)
-        fs.readdir(prefixDir, function(err, files){
-            files.forEach(function(filename){
-                filename = path.join(prefixDir, filename)
-                if (minimatch(filename, pattern)){
-                    gw.add(filename)
-                }
-            })
-            gw.popTask()
-        })
-        gw.addFile(prefixDir)
-    }
-
-    gw.addFile = function(filename){
-        if (gw.watchedFiles[filename]) return
-        gw.pushTask()
-        gw.watchedFiles[filename] = fs.watch(filename, function(){
-            gw.onFileAccessed(filename)
-        })
-        gw.popTask()
-    }
-
-    gw.pushTask = function(){
-        gw.pendingTasks++
-    }
-
-    gw.popTask = function(){
-        gw.pendingTasks--
-        gw.checkTasks()
-    }
-
-    gw.checkTasks = function(){
-        if (gw.pendingTasks === 0){
-            process.nextTick(function(){
-                gw.emit('ready')
-            })
-        }
-    }
-
-    gw.onFileAccessed = function(filename){
-        gw.emit('change', filename)
-    }
-
+    var gw = clone(GW)
+    gw.initialize()
     return gw
-
 }
